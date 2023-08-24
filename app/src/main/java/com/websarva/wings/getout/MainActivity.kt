5ebfb663ce0e43283d9de516abd64952f65ed0a8
ryohamag <Ryojune21@gmail.com>
2023-08-24 18:44:10 +0900

    月グラフへの画面遷移成功。戻るボタン未実装

--- app/src/main/java/com/websarva/wings/getout/MainActivity.kt
+++ app/src/main/java/com/websarva/wings/getout/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.websarva.wings.getout
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btMonth = findViewById<Button>(R.id.btMonth)
+        //ボタンクリックのリスナーを設定。
+        btMonth.setOnClickListener {
+            //インテントオブジェクトを生成。
+            val intent2ChartMonth = Intent(this@MainActivity, ChartMonthActivity::class.java)
+            // 月グラフ画面の起動。
+            startActivity(intent2ChartMonth)
+        }
     }
 }
