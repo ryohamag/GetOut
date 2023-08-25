02f9a803abc29871562b4f0676c5ce0d50a0ea98
ryohamag <Ryojune21@gmail.com>
2023-08-25 12:31:28 +0900

    週グラフ実装

--- app/src/main/java/com/websarva/wings/getout/ChartMonthActivity.kt
+++ app/src/main/java/com/websarva/wings/getout/ChartMonthActivity.kt
@@ -1,58 +1,101 @@
 package com.websarva.wings.getout
 
-import android.graphics.Color
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import com.github.mikephil.charting.charts.BarChart
+import com.github.mikephil.charting.components.XAxis
 import com.github.mikephil.charting.data.BarData
 import com.github.mikephil.charting.data.BarDataSet
 import com.github.mikephil.charting.data.BarEntry
+import com.github.mikephil.charting.formatter.ValueFormatter
 import com.github.mikephil.charting.interfaces.datasets.IBarDataSet
-import com.github.mikephil.charting.charts.BarChart
-import com.github.mikephil.charting.formatter.IndexAxisValueFormatter
+import java.text.SimpleDateFormat
+import java.util.*
 
 class ChartMonthActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chart_month)
 
-        //表示用サンプルデータの作成
-        val x = listOf<Float>(1f, 2f, 3f, 4f, 6f, 7f, 8f, 9f, 10f, 11f, 12f)//X軸データ
-        val y = x.map{it*it}//Y軸データ（X軸の2乗）
+        val barChart: BarChart = findViewById(R.id.barChart)
+        // X 軸ごとの Y 軸
+        val entries: MutableList<Int> = mutableListOf(
+            105,
+            129,
+            85,
+            104,
+            52,
+            84,
+            114
+        )
 
-        //Entryにデータ格納
-        val entryList = mutableListOf<BarEntry>()
-        for(i in x.indices){
-            entryList.add(
-                BarEntry(x[i], y[i])
+        // X 軸のタイムスタンプ
+        val entriesTimestampMills: MutableList<String> = mutableListOf(
+            "Jan",  // 2021/03/01 00:00:00
+            "Feb",  // 2021/03/02 00:00:00
+            "Mar",  // 2021/03/03 00:00:00
+            "Apr",  // 2021/03/04 00:00:00
+            "May",  // 2021/03/05 00:00:00
+            "Jun",  // 2021/03/06 00:00:00
+            "July"   // 2021/03/07 00:00:00
+        )
+
+        // グラフに描画するデータの設定
+        val entryList = entries.mapIndexed { index, entry ->
+            BarEntry(
+                index.toFloat(),    // X 軸 ここに渡すのはあくまで 0, 1, 2... という index
+                entry.toFloat()     // Y 軸
             )
         }
 
+        val barDataSet = BarDataSet(entryList, "barChart")
+        barDataSet.setDrawValues(false)
 
-        //DataSetにデータ格納
-        val barDataSet = BarDataSet(entryList, "square")
-        //DataSetのフォーマット指定
-        barDataSet.color = Color.BLUE
+        barChart.data = BarData(mutableListOf<IBarDataSet>(barDataSet))
+        barChart.setDrawGridBackground(false)
+        barChart.description.isEnabled = false
 
-
-        //BarDataにBarDataSet格納
-        val barData = BarData(barDataSet)
-        //BarChartにBarData格納
-        val barChart = findViewById<BarChart>(R.id.barChartExample)
-        barChart.data = barData
-        barChart.legend.isEnabled= false
-        barChart.description.text = ""
-        barChart.setScaleEnabled(false)
-        barChart.setPinchZoom(false)
-        //Chartのフォーマット指定
-        //X軸の設定
-        val labels = x.map { it.toInt().toString() }.toTypedArray()
-        barChart.xAxis.apply {
-            isEnabled = true
-            textColor = Color.BLACK
-            valueFormatter = IndexAxisValueFormatter(labels)
-
+        barChart.legend.apply {
+            isEnabled = false
         }
-        //barchart更新
+
+        // X 軸のフォーマッター
+        val xAxisFormatter = object : ValueFormatter() {
+            private var simpleDateFormat: SimpleDateFormat =
+                SimpleDateFormat("M/d", Locale.getDefault())
+
+            override fun getFormattedValue(value: Float): String {
+                // value には 0, 1, 2... という index が入ってくるので
+                // index からタイムスタンプを取得する
+                val timestampMills = entriesTimestampMills[value.toInt()]
+                val date = Date(timestampMills)
+                return simpleDateFormat.format(date)
+            }
+        }
+
+        // X 軸の設定
+        barChart.xAxis.apply {
+            position = XAxis.XAxisPosition.BOTTOM
+            valueFormatter = xAxisFormatter
+            setDrawGridLines(false)
+        }
+
+        // Y 軸（左）の設定
+        barChart.axisLeft.apply {
+            setDrawGridLines(true)
+            axisMinimum = 0f
+        }
+
+        // Y 軸（右）の設定
+        barChart.axisRight.apply {
+            isEnabled = false
+        }
+
+        // グラフ描画
         barChart.invalidate()
+
     }
 }
+
--- app/src/main/java/com/websarva/wings/getout/ChartWeekActivity.kt
+++ app/src/main/java/com/websarva/wings/getout/ChartWeekActivity.kt
@@ -5,49 +5,99 @@ import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.github.mikephil.charting.charts.BarChart
+import com.github.mikephil.charting.components.XAxis
 import com.github.mikephil.charting.data.BarData
 import com.github.mikephil.charting.data.BarDataSet
 import com.github.mikephil.charting.data.BarEntry
+import com.github.mikephil.charting.formatter.ValueFormatter
 import com.github.mikephil.charting.interfaces.datasets.IBarDataSet
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 class ChartWeekActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chart_week)
 
-        //表示用サンプルデータの作成
-        val x = listOf<Float>(1f, 2f, 3f, 4f, 6f, 7f, 8f, 9f)//X軸データ
-        val y = x.map{it*it}//Y軸データ（X軸の2乗）
+        val barChart: BarChart = findViewById(R.id.barChart)
+        // X 軸ごとの Y 軸
+        val entries: MutableList<Int> = mutableListOf(
+            105,
+            129,
+            85,
+            104,
+            52,
+            84,
+            114
+        )
 
-        //Entryにデータ格納
-        val entryList = mutableListOf<BarEntry>()
-        for(i in x.indices){
-            entryList.add(
-                BarEntry(x[i], y[i])
+        // X 軸のタイムスタンプ
+        val entriesTimestampMills: MutableList<Long> = mutableListOf(
+            1614524400000,  // 2021/03/01 00:00:00
+            1614610800000,  // 2021/03/02 00:00:00
+            1614697200000,  // 2021/03/03 00:00:00
+            1614783600000,  // 2021/03/04 00:00:00
+            1614870000000,  // 2021/03/05 00:00:00
+            1614956400000,  // 2021/03/06 00:00:00
+            1615042800000   // 2021/03/07 00:00:00
+        )
+
+        // グラフに描画するデータの設定
+        val entryList = entries.mapIndexed { index, entry ->
+            BarEntry(
+                index.toFloat(),    // X 軸 ここに渡すのはあくまで 0, 1, 2... という index
+                entry.toFloat()     // Y 軸
             )
         }
 
-        //BarDataSetのリスト
-        val barDataSets = mutableListOf<IBarDataSet>()
-        //DataSetにデータ格納
-        val barDataSet = BarDataSet(entryList, "square")
-        //DataSetのフォーマット指定
-        barDataSet.color = Color.BLUE
-        //リストに格納
-        barDataSets.add(barDataSet)
+        val barDataSet = BarDataSet(entryList, "barChart")
+        barDataSet.setDrawValues(false)
 
-        //BarDataにBarDataSet格納
-        val barData = BarData(barDataSets)
-        //BarChartにBarData格納
-        val barChart = findViewById<BarChart>(R.id.barChartExample)
-        barChart.data = barData
-        //Chartのフォーマット指定
-        //X軸の設定
-        barChart.xAxis.apply {
-            isEnabled = true
-            textColor = Color.BLACK
+        barChart.data = BarData(mutableListOf<IBarDataSet>(barDataSet))
+        barChart.setDrawGridBackground(false)
+        barChart.description.isEnabled = false
+
+        barChart.legend.apply {
+            isEnabled = false
         }
-        //barchart更新
+
+        // X 軸のフォーマッター
+        val xAxisFormatter = object : ValueFormatter() {
+            private var simpleDateFormat: SimpleDateFormat =
+                SimpleDateFormat("M/d", Locale.getDefault())
+
+            override fun getFormattedValue(value: Float): String {
+                // value には 0, 1, 2... という index が入ってくるので
+                // index からタイムスタンプを取得する
+                val timestampMills = entriesTimestampMills[value.toInt()]
+                val date = Date(timestampMills)
+                return simpleDateFormat.format(date)
+            }
+        }
+
+        // X 軸の設定
+        barChart.xAxis.apply {
+            position = XAxis.XAxisPosition.BOTTOM
+            valueFormatter = xAxisFormatter
+            setDrawGridLines(false)
+        }
+
+        // Y 軸（左）の設定
+        barChart.axisLeft.apply {
+            setDrawGridLines(true)
+            axisMinimum = 0f
+        }
+
+        // Y 軸（右）の設定
+        barChart.axisRight.apply {
+            isEnabled = false
+        }
+
+        // グラフ描画
         barChart.invalidate()
+
+
+
     }
 }
