e567e6c2538cc3a3438dce017e5a76d7cfc4172b
ryohamag <Ryojune21@gmail.com>
2023-08-25 10:41:03 +0900

    グラフ作成中

--- app/src/main/java/com/websarva/wings/getout/ChartMonthActivity.kt
+++ app/src/main/java/com/websarva/wings/getout/ChartMonthActivity.kt
@@ -8,6 +8,7 @@ import com.github.mikephil.charting.data.BarDataSet
 import com.github.mikephil.charting.data.BarEntry
 import com.github.mikephil.charting.interfaces.datasets.IBarDataSet
 import com.github.mikephil.charting.charts.BarChart
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter
 
 class ChartMonthActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,7 +16,7 @@ class ChartMonthActivity : AppCompatActivity() {
         setContentView(R.layout.activity_chart_month)
 
         //表示用サンプルデータの作成
-        val x = listOf<Float>(1f, 2f, 3f, 4f, 6f, 7f, 8f, 9f)//X軸データ
+        val x = listOf<Float>(1f, 2f, 3f, 4f, 6f, 7f, 8f, 9f, 10f, 11f, 12f)//X軸データ
         val y = x.map{it*it}//Y軸データ（X軸の2乗）
 
         //Entryにデータ格納
@@ -26,27 +27,30 @@ class ChartMonthActivity : AppCompatActivity() {
             )
         }
 
-        //BarDataSetのリスト
-        val barDataSets = mutableListOf<IBarDataSet>()
+
         //DataSetにデータ格納
         val barDataSet = BarDataSet(entryList, "square")
         //DataSetのフォーマット指定
         barDataSet.color = Color.BLUE
-        //リストに格納
-        barDataSets.add(barDataSet)
+
 
         //BarDataにBarDataSet格納
-        val barData = BarData(barDataSets)
+        val barData = BarData(barDataSet)
         //BarChartにBarData格納
         val barChart = findViewById<BarChart>(R.id.barChartExample)
         barChart.data = barData
         barChart.legend.isEnabled= false
         barChart.description.text = ""
+        barChart.setScaleEnabled(false)
+        barChart.setPinchZoom(false)
         //Chartのフォーマット指定
         //X軸の設定
+        val labels = x.map { it.toInt().toString() }.toTypedArray()
         barChart.xAxis.apply {
             isEnabled = true
             textColor = Color.BLACK
+            valueFormatter = IndexAxisValueFormatter(labels)
+
         }
         //barchart更新
         barChart.invalidate()
