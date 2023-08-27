bcb034c331c20f82edc5afd87aa64b4a9305146b
ryohamag <Ryojune21@gmail.com>
2023-08-27 10:44:34 +0900

    グラフ遷移に苦戦中

--- app/src/main/java/com/websarva/wings/getout/ChartWeekActivity.kt
+++ app/src/main/java/com/websarva/wings/getout/ChartWeekActivity.kt
@@ -4,6 +4,8 @@ import android.annotation.SuppressLint
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import com.github.mikephil.charting.charts.BarChart
 import com.github.mikephil.charting.components.XAxis
 import com.github.mikephil.charting.data.BarData
@@ -16,19 +18,28 @@ import java.util.Date
 import java.util.Locale
 
 class ChartWeekActivity : AppCompatActivity() {
+    var Sun: Long = 1693753200000  //2023/09/03 00:00:00
+    var Mon: Long = 1693839600000  //2023/09/04 00:00:00
+    var Tue: Long = 1693926000000  //2023/09/05 00:00:00
+    var Wed: Long = 1694012400000  //2023/09/06 00:00:00
+    var Thu: Long = 1694098800000  //2023/09/07 00:00:00
+    var Fri: Long = 1694185200000  //2023/09/08 00:00:00
+    var Sat: Long = 1694271600000  //2023/09/09 00:00:00
+
+    var SunData = 105
+    var MonData = 129
+    var TueData = 85
+    var WedData = 104
+    var ThuData = 52
+    var FriData = 84
+    var SatData = 114
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chart_week)
 
         val barChart: BarChart = findViewById(R.id.barChart)
-        //ここにデータをぶち込む
-        var SunData = 105
-        var MonData = 129
-        var TueData = 85
-        var WedData = 104
-        var ThuData = 52
-        var FriData = 84
-        var SatData = 114
+
         // X 軸ごとの Y 軸
         val entries: MutableList<Int> = mutableListOf(
             SunData,
@@ -39,14 +50,6 @@ class ChartWeekActivity : AppCompatActivity() {
             FriData,
             SatData
         )
-
-        var Sun = 1693753200000  //2023/09/03 00:00:00
-        var Mon = 1693839600000  //2023/09/04 00:00:00
-        var Tue = 1693926000000  //2023/09/05 00:00:00
-        var Wed = 1694012400000  //2023/09/06 00:00:00
-        var Thu = 1694098800000  //2023/09/07 00:00:00
-        var Fri = 1694185200000  //2023/09/08 00:00:00
-        var Sat = 1694271600000  //2023/09/09 00:00:00
 
 
         // X 軸のタイムスタンプ
@@ -114,7 +117,33 @@ class ChartWeekActivity : AppCompatActivity() {
         // グラフ描画
         barChart.invalidate()
 
+        //リスナクラスのインスタンスを生成。
+        val listener = ToWeekListener(this)
+        //「先週へ」ボタンであるButtonオブジェクトを取得。
+        val btToLastWeek = findViewById<Button>(R.id.btToLastWeek)
+        //「先週へ」ボタンにリスナを設定。
+        btToLastWeek.setOnClickListener(listener)
+    }
 
-
+    //ボタンをクリックしたときのリスナクラス
+    private inner class ToWeekListener(private val activity: ChartWeekActivity): View.OnClickListener {
+        override fun onClick(view: View){
+            val barChart: BarChart = activity.findViewById(R.id.barChart)
+            //idのR値に応じて処理を分岐。
+            when(view.id){
+                //「先週へ」ボタンの場合
+                R.id.btToLastWeek -> {
+                    activity.Sun -= 604800000
+                    activity.Mon -= 604800000
+                    activity.Tue -= 604800000
+                    activity.Wed -= 604800000
+                    activity.Thu -= 604800000
+                    activity.Fri -= 604800000
+                    activity.Sat -= 604800000
+                    // グラフ描画
+                    barChart.invalidate()
+                }
+            }
+        }
     }
 }
